7d45f21dd06b1fff98e046254c6095bdfeb8998b
rianaoktavia <rianaoktavia3333@gmail.com>
2021-12-05 12:17:01 +0700

    pertemuan 9

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -134,7 +134,7 @@ class Autentifikasi extends CI_Controller
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 0,
+                'is_active' => 1,
                 'tanggal_input' => time()
             ];
             $this->ModelUser->simpanData($data); //menggunakan model
